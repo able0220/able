11d16ebebfb41ebdf59fc514d4fd324537667c9a
able0220 <73169401+able0220@users.noreply.github.com>
2021-08-19 14:27:17 +0900

    Update sketch_may18a.ino

--- sketch_may18a.ino
+++ sketch_may18a.ino
@@ -14,11 +14,12 @@ void loop() {
   if(Serial.available()){
 
     data = Serial.read();
-    if(data == '1')
+    if(data == '1')  // 티처블머신에서 사진이 1번이면 {   } 안에 내용 실행
     {
       analogWrite(9,100);
       analogWrite(10,0);
       analogWrite(11,0);
+      // 9번 10번 11번 에 불빛 조합으로 색 결정
     }
     else if(data == '2')
     {
